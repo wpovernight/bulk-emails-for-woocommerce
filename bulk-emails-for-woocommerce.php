5a55436776017fe156b6dfee3dd1d6f0a060d612
Alexandre Faustino <alexmigf@gmail.com>
2022-08-11 10:03:00 +0100

    Set request action to manual email to bypass reminder triggers validation

--- bulk-emails-for-woocommerce.php
+++ bulk-emails-for-woocommerce.php
@@ -167,7 +167,8 @@ class WPO_BEWC {
 
 		// Reminder emails
 		if ( strpos( $email_to_send, 'wcsre_' ) !== false && class_exists( 'WPO_WC_Smart_Reminder_Emails' ) ) {
-			$email_id = str_replace( 'wcsre_', '', $email_to_send );
+			$_REQUEST['action'] = 'wpo_wcsre_send_manual_email'; // set it to be manual email to bypass triggers validation
+			$email_id           = str_replace( 'wcsre_', '', $email_to_send );
 			WPO_WCSRE()->functions->send_emails( $order_id, $email_id );
 		
 		// Regular emails
